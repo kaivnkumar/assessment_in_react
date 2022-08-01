8e3d9d67e1467c63e3c67e12fa31592725d5a39f
kavinkumar <kavinkumars218@gmail.com>
2022-08-01 14:02:27 +0530

    Kavinkumar displayed the Api

--- src/Components/Fetchapi/FetchApi.jsx
+++ src/Components/Fetchapi/FetchApi.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react'
+import React, {useEffect,useState} from 'react'
 
 function FetchApi() {
+    const [details, setDetails] = useState(JSON.parse(localStorage.getItem('personData')||"[]"));
     useEffect(()=> {
         fetch('https://randomuser.me/api/?results=10')
             .then((info) => {
-                info.json().then((data)=> {
-                    console.log(data);
+                info.json().then((details) => {
+                    console.log(details.results);
+                    setDetails(details.results);
+                    localStorage.setItem('personInfo',JSON.stringify(details.results))
+
                 })
             });
         },[]
     );
-
   return (
-    <div>FetchApi</div>
+    <div>
+        {
+            (details.length > 0) && details.map((object,index)=>(
+                <div key={index}>
+                    <img src={object.picture.large} /><br />
+                    <span>{object.name.first}</span><br />
+                    <span>{object.dob.date}</span><br />
+                    <span>{object.gender}</span><br />
+                    <span>{object.phone}</span><br />
+                    <span>{object.email}</span><br />
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
